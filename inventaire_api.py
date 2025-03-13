d7119ae83771186d4744dc775c60e7c77c306713
Sarrablo <gsarrablo@gmail.com>
2025-03-13 15:05:57 +0100

    Migration to class model

--- inventaire_api.py
+++ inventaire_api.py
@@ -1,149 +1,150 @@
-"""Unnoficial Inventaire Api"""
 import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from unidecode import unidecode
 
-logger = logging.getLogger('inventaire_api')
-logger.setLevel(logging.DEBUG)
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-logger.addHandler(ch)
 
-# Define a custom user agent
-MY_USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-"AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36")
-# Set up Chrome options
-chrome_options = Options()
-# Set the custom User-Agent
-chrome_options.add_argument(f"--user-agent={MY_USER_AGENT}")
-chrome_options.add_argument("--lang=es")
-chrome_options.add_argument("--no-sandbox")
-# Create a new instance of ChromeDriver with the desired options
-driver = webdriver.Chrome(options=chrome_options)
-logger.info("Created Driver")
+class InventaireApi:
+    def __init__(self):
+        self.logger = logging.getLogger('inventaire_api')
+        self.logger.setLevel(logging.DEBUG)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
+        self.MY_USER_AGENT = (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
+        )
 
-def wait_until_loaded(by_selector, selector):
-    """Wait unitl the desired element is loaded"""
-    loaded = None
-    while loaded is None:
-        try:
-            loaded = driver.find_element(by_selector, selector)
-        except Exception:
-            pass
-    return True
+        chrome_options = Options()
+        chrome_options.add_argument(f"--user-agent={self.MY_USER_AGENT}")
+        chrome_options.add_argument("--lang=es")
+        chrome_options.add_argument("--no-sandbox")
 
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.logger.info("Created Driver")
 
-def login(user, password):
-    """Loggin into the inventaire web"""
-    logger.info("Loggin..")
-    driver.get("https://inventaire.io/login")
-    wait_until_loaded(By.ID, 'username')
-    _user = driver.find_element(By.ID, 'username')
-    _pwd = driver.find_element(By.NAME, 'password')
-    _show_pwd = driver.find_element(By.CLASS_NAME,
-                                    'show-password.svelte-6g1eqi')
-    _submit_button = driver.find_element(By.ID, 'login')
-    _user.send_keys(user)
-    _pwd.send_keys(password)
-    _show_pwd.click()
-    _submit_button.click()
-    logged = None
-    while logged is None:
-        driver.implicitly_wait(1)
-        try:
-            logged = driver.find_element(
-                By.CLASS_NAME, 'username.respect-case.svelte-19zdlyb')
-            logger.info("Logging Success")
-            return True
-        except:
-            pass
-        try:
-            logged = driver.find_element(By.CLASS_NAME,
-                                         'flash.error.svelte-btfyjl')
-            logger.info("Loging Failed")
-            return False
-        except:
-            pass
+    def wait_until_loaded(self, by_selector, selector):
+        """Wait unitl the desired element is loaded"""
+        loaded = None
+        while loaded is None:
+            try:
+                loaded = self.driver.find_element(by_selector, selector)
+            except Exception:
+                pass
+        return True
 
+    def login(self, user, password):
+        """Loggin into the inventaire web"""
+        self.logger.info("Logging in...")
+        self.driver.get("https://inventaire.io/login")
+        self.wait_until_loaded(By.ID, 'username')
 
-def search_by_isbn(isbn):
-    """Find if a isbn exists in inventaire"""
-    driver.get("https://inventaire.io")
-    wait_until_loaded(By.XPATH, "//input[@class='svelte-qzi9hf']")
-    logger.info("Search page loaded")
-    _search_input = driver.find_element(By.XPATH,
-                                        "//input[@class='svelte-qzi9hf']")
-    _search_input.send_keys(isbn)
-    search_result = None
-    while search_result is None:
-        driver.implicitly_wait(1)
-        try:
-            search_result = driver.find_element(By.CLASS_NAME, 'svelte-9gl3v7')
-            logger.info("Search Success")
-            _search_link = search_result.find_element(By.XPATH, "//a[@class='svelte-9gl3v7']")
-            return _search_link.get_attribute('href')
-        except:
-            pass
-        try:
-            search_result = driver.find_element(By.CLASS_NAME,
-                                                'no-result.svelte-qzi9hf')
-            logger.info("Search Failed")
-            return False
-        except:
-            pass
+        _user = self.driver.find_element(By.ID, 'username')
+        _pwd = self.driver.find_element(By.NAME, 'password')
+        _show_pwd = self.driver.find_element(By.CLASS_NAME, 'show-password.svelte-6g1eqi')
+        _submit_button = self.driver.find_element(By.ID, 'login')
 
+        _user.send_keys(user)
+        _pwd.send_keys(password)
+        _show_pwd.click()
+        _submit_button.click()
 
-def create_work(title, author):
-    """Create a work (book)"""
-    driver.get("https://inventaire.io/entity/new?type=work")
-    wait_until_loaded(By.CLASS_NAME, "column.svelte-1z0jooq")
-    logger.info("Create work loaded")
-    _title = driver.find_element(By.XPATH, "//input[@class='svelte-o6gvsq']")
-    _title.send_keys(title)
-    _save_button = driver.find_element(By.CLASS_NAME,
-                                       'tiny-button.save.svelte-1lv9oa')
-    _save_button.click()
-    for elem in driver.find_elements(By.CLASS_NAME,
-                                     "editor-section.svelte-1j1ofcl"):
-        source = elem.get_attribute('innerHTML')
-        if "Autor" in source:
-            _author_button = elem.find_element(
-                By.CLASS_NAME,
-                'add-value.tiny-button.soft-grey.svelte-1j1ofcl')
-            _author_element = elem
-    _author_button.click()
-    _author_input = _author_element.find_element(
-        By.XPATH, "//input[@class='svelte-1u25yab']")
-    _author_input.send_keys(author)
-    wait_until_loaded(By.CLASS_NAME, 'svelte-dynnwx')
-    logger.info("Author autocomplete loaded")
-    _autocomplete = _author_element.find_element(
-        By.CLASS_NAME, 'autocomplete.svelte-1u25yab')
-    _author_found = False
-    for auth in _autocomplete.find_elements(By.XPATH,
-                                            "//li[@class='svelte-dynnwx']"):
-        _author_name = unidecode(auth.find_element(
-            By.CLASS_NAME,
-            'label.svelte-dynnwx').get_attribute('innerHTML'))
-        if _author_name in author:
-            _author_found = True
-            logger.info("Author %s found", author)
-            auth.click()
-            break
+        logged = None
+        while logged is None:
+            self.driver.implicitly_wait(1)
+            try:
+                logged = self.driver.find_element(By.CLASS_NAME, 'username.respect-case.svelte-19zdlyb')
+                self.logger.info("Login Success")
+                return True
+            except:
+                pass
+            try:
+                logged = self.driver.find_element(By.CLASS_NAME, 'flash.error.svelte-btfyjl')
+                self.logger.info("Login Failed")
+                return False
+            except:
+                pass
 
-    if not _author_found:
-        logger.info("Author %s not found, creating...", author)
-        _autocomplete.find_element(By.CLASS_NAME,
-                                   'create.svelte-1u25yab').click()
-    logger.info("Author OK")
-    _submit_work_button = driver.find_element(
-        By.CLASS_NAME, 'light-blue-button.svelte-1z0jooq')
-    _submit_work_button.click()
-    logger.info("Submited")
-    return True
+    def search_by_isbn(self, isbn):
+        """Find if a isbn exists in inventaire"""
+        self.driver.get("https://inventaire.io")
+        self.wait_until_loaded(By.XPATH, "//input[@class='svelte-qzi9hf']")
+        self.logger.info("Search page loaded")
+
+        _search_input = self.driver.find_element(By.XPATH, "//input[@class='svelte-qzi9hf']")
+        _search_input.send_keys(isbn)
+
+        search_result = None
+        while search_result is None:
+            self.driver.implicitly_wait(1)
+            try:
+                search_result = self.driver.find_element(By.CLASS_NAME, 'svelte-9gl3v7')
+                self.logger.info("Search Success")
+                _search_link = search_result.find_element(By.XPATH, "//a[@class='svelte-9gl3v7']")
+                return _search_link.get_attribute('href')
+            except:
+                pass
+            try:
+                search_result = self.driver.find_element(By.CLASS_NAME, 'no-result.svelte-qzi9hf')
+                self.logger.info("Search Failed")
+                return False
+            except:
+                pass
+
+    def create_work(self, title, author):
+        """Create a work (book)"""
+        self.driver.get("https://inventaire.io/entity/new?type=work")
+        self.wait_until_loaded(By.CLASS_NAME, "column.svelte-1z0jooq")
+        self.logger.info("Create work loaded")
+
+        _title = self.driver.find_element(By.XPATH, "//input[@class='svelte-o6gvsq']")
+        _title.send_keys(title)
+
+        _save_button = self.driver.find_element(By.CLASS_NAME, 'tiny-button.save.svelte-1lv9oa')
+        _save_button.click()
+
+        for elem in self.driver.find_elements(By.CLASS_NAME, "editor-section.svelte-1j1ofcl"):
+            source = elem.get_attribute('innerHTML')
+            if "Autor" in source:
+                _author_button = elem.find_element(
+                    By.CLASS_NAME, 'add-value.tiny-button.soft-grey.svelte-1j1ofcl')
+                _author_element = elem
+                break
+
+        _author_button.click()
+        _author_input = _author_element.find_element(By.XPATH, "//input[@class='svelte-1u25yab']")
+        _author_input.send_keys(author)
+
+        self.wait_until_loaded(By.CLASS_NAME, 'svelte-dynnwx')
+        self.logger.info("Author autocomplete loaded")
+
+        _autocomplete = _author_element.find_element(By.CLASS_NAME, 'autocomplete.svelte-1u25yab')
+        _author_found = False
+
+        for auth in _autocomplete.find_elements(By.XPATH, "//li[@class='svelte-dynnwx']"):
+            _author_name = unidecode(auth.find_element(By.CLASS_NAME, 'label.svelte-dynnwx').get_attribute('innerHTML'))
+            if _author_name in author:
+                _author_found = True
+                self.logger.info("Author %s found", author)
+                auth.click()
+                break
+
+        if not _author_found:
+            self.logger.info("Author %s not found, creating...", author)
+            _autocomplete.find_element(By.CLASS_NAME, 'create.svelte-1u25yab').click()
+
+        self.logger.info("Author OK")
+        _submit_work_button = self.driver.find_element(By.CLASS_NAME, 'light-blue-button.svelte-1z0jooq')
+        _submit_work_button.click()
+        self.logger.info("Submitted")
+        return True
+
+    def close(self):
+        self.driver.quit()
+        self.logger.info("Driver closed")
+
